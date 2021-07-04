13caefdac1ad64f7a6a992cf34410e63d0531839
olyamin <olyamin@sinergo.ru>
2021-07-05 00:38:42 +0300

    fix decorators log

--- src/main/java/com/oleglmn/knowledgebase/patterns/structural/decorator/CanvasImpl.java
+++ src/main/java/com/oleglmn/knowledgebase/patterns/structural/decorator/CanvasImpl.java
@@ -3,6 +3,6 @@ package com.oleglmn.knowledgebase.patterns.structural.decorator;
 public class CanvasImpl implements Canvas {
     @Override
     public void draw() {
-        System.out.println("Start drowing ...");
+        System.out.println("Start drowing earth...");
     }
 }
